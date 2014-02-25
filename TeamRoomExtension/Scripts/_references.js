5f00e10992b19bb1f0cd63bf7ccd12b725c25f94
josh <josh@jschlesinger.net>
2014-02-24 20:29:02 -0600

    added basic ng and bootstrap stuff

--- TeamRoomExtension/Scripts/_references.js
+++ TeamRoomExtension/Scripts/_references.js
@@ -1,1 +1,2 @@
-﻿/// <reference path="jquery-2.1.0.js" />
+﻿/// <reference path="angular.js" />
+/// <reference path="popup.js" />
--- TeamRoomExtension/Scripts/popup.js
+++ TeamRoomExtension/Scripts/popup.js
@@ -1,13 +1,22 @@
-$(document).ready(function () {
-    var messageCount = 0;
+//$(document).ready(function () {
+//    var messageCount = 0;
 
-    $.getJSON("https://defiantcode.visualstudio.com/DefaultCollection/_apis/chat/rooms/13/messages", {}, function (data) {
-        messageCount = data.count;
-        $("#main").text("There are " + data.count + " messages in this room");
+//    $.getJSON("https://defiantcode.visualstudio.com/DefaultCollection/_apis/chat/rooms/13/messages", {}, function (data) {
+//        messageCount = data.count;
+//        $("#main").text("There are " + data.count + " messages in this room");
 
-        var notification = webkitNotifications.createNotification('', 'Team Room', 'There are ' + data.count + ' messages in this room');
+//        var notification = webkitNotifications.createNotification('', 'Team Room', 'There are ' + data.count + ' messages in this room');
 
-        // Then show the notification.
-        notification.show();
-    });
+//        // Then show the notification.
+//        notification.show();
+//    });
+//});
+
+var app = angular.module('app', []);
+app.controller('RoomNotificationsCtrl', function ($scope) {
+    $scope.rooms = [
+        { 'name': 'Room1', 'newMessages': 10 },
+        { 'name': 'Room2', 'newMessages': 4 },
+        { 'name': 'Room3', 'newMessages': 0 }
+    ];
 });
